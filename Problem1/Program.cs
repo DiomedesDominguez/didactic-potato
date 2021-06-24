8fc1150233386b0e0335ecf89de3d8ba87e0053f
Diomedes <diomedes.dominguez@dnmoft.com>
2021-06-24 08:32:26 -0400

    Problem 1 & Problem 2

---
+++ Problem1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = Enumerable.Range(1, 1000).Where(x => x % 3 == 0 || x % 5 == 0).Sum();
+            Console.WriteLine(x);
+        }
+    }
+}
---
+++ Problem2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long x = Fibonacci().TakeWhile(x => x < 4000000L)
+                          .Where(x => x % 2L == 0L)
+                          .Sum();
+
+            Console.WriteLine(x);
+        }
+
+        public static IEnumerable<long> Fibonacci()
+        {
+            long current = 0;
+            long next = 1;
+            while (true)
+            {
+                yield return current;
+                long temp = next;
+                next = current + next;
+                current = temp;
+            }
+        }
+
+    }
+}
